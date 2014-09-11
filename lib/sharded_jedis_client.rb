551401298c77e884cb51887c21f62486b278f6d0
Dermot A. Harris <dermot.harris@teamaol.com>
2014-09-11 14:20:26 -0400

    Updated version of the Jedis jar, changed to use a connection pool, and support for authentication.

--- lib/sharded_jedis_client.rb
+++ lib/sharded_jedis_client.rb
@@ -1,24 +1,50 @@
-require 'delegate'
 require 'jruby/jedis'
+
 #A wrapper around the Redis Jedis client.  The Jedsis client supports sharding.
 #
 #Example:
 #
 #
 #  hosts = ['qa-cache-mstr-rpt.advertising.aol.com:6310','qa-cache-mstr-rpt.advertising.aol.com:6320','qa-cache-mstr-rpt.advertising.aol.com:6330']
-#  
+#
 #  redis =  ShardedJedisClient.new(hosts)
 #
 #  redis.get('some_key_271777')
 #
-class ShardedJedisClient < SimpleDelegator
+class ShardedJedisClient
+
   def initialize(hosts =[])
-    super
+    config = Java::OrgApacheCommonsPool2Impl::GenericObjectPoolConfig.new
+    config.test_on_borrow = true
     shards = java.util.ArrayList.new
-    hosts.map{|h| h.split(':')}.each do |host,port|
-      shards << Java::RedisClientsJedis::JedisShardInfo.new(host, port.to_i)
+    hosts.each do |host| 
+      host,port,password = host.split(':')
+      shard = Java::RedisClientsJedis::JedisShardInfo.new(host, port.to_i, password)
+      if password
+        shard.set_password(password)
+      end
+      shards << shard
     end
-    @redis = Java::RedisClientsJedis::ShardedJedis.new(shards)
-    __setobj__(@redis)
+    @pool = Java::RedisClientsJedis::ShardedJedisPool.new(config, shards);
   end
+
+  def method_missing(method, *arguments, &block)
+    client = @pool.get_resource
+    resp = client.send(method, *arguments, &block)
+    @pool.return_resource(client)
+    resp
+  end
+  
 end
+
+#ShardedJedisPool pool = new ShardedJedisPool(new Config(), shards);
+#ShardedJedis jedis = pool.getResource();
+#jedis.set("a", "foo");
+#.... // do your work here
+#pool.returnResource(jedis);
+#.... // a few moments later
+#ShardedJedis jedis2 = pool.getResource();
+#jedis.set("z", "bar");
+#pool.returnResource(jedis);
+#pool.destroy()
+
